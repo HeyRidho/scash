cb02aee648722c0ae4532f9e9a5827acc6b91ede
HeyRidho <sugity.ridho@gmail.com>
2022-09-18 18:00:24 +0700

    fix link

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,7 +3,6 @@ import { Outlet } from "react-router-dom";
 const Home = () => {
     return (
         <section id="home">
-            <h1>Hello Home</h1>
             <Outlet />
         </section>
     )
